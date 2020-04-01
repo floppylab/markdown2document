98fef66114ee32f4d00621d39381a4357c70d076
bravemaster619 <bravemaster619@hotmail.com>
2020-04-02 04:08:22 +0800

    Add style missing check and default style

--- src/main/java/com/floppylab/markdown2document/generator/DocumentGenerator.java
+++ src/main/java/com/floppylab/markdown2document/generator/DocumentGenerator.java
@@ -1,8 +1,11 @@
 package com.floppylab.markdown2document.generator;
 
 import com.floppylab.markdown2document.domain.Document;
+import com.floppylab.markdown2document.domain.Input;
 import com.floppylab.markdown2document.domain.Output;
 import com.floppylab.markdown2document.exception.DocumentGeneratorException;
+import java.util.ArrayList;
+import java.util.List;
 
 public abstract class DocumentGenerator {
 
@@ -14,4 +17,18 @@ public abstract class DocumentGenerator {
      * @throws DocumentGeneratorException if a low-level problem occurs
      */
     public abstract Output generate(Document document) throws DocumentGeneratorException;
+
+    protected boolean isStyleMissing(Document document) {
+        return document.getStyles() == null || document.getStyles().isEmpty();
+    }
+
+    protected Document addDefaultStyle(Document document) {
+        List<Input> inputs = document.getStyles();
+        if (inputs == null) {
+            inputs = new ArrayList<Input>();
+        }
+        inputs.add(Input.DEFAULT);
+        document.setStyles(inputs);
+        return document;
+    }
 }
